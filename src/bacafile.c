40d59d6adba69bce25cb088b6739f365121c5246
Fakhrurrida <fakhrurridawidodo@gmail.com>
2019-11-14 12:11:30 +0700

    Update bacafile.c

--- src/bacafile.c
+++ src/bacafile.c
@@ -2,45 +2,50 @@
 #include "../include/bacafile.h"
 #include "../include/mesinkata.h"
 #include "../include/state.h"
-
+#include "../include/matriks.h"
 
 static FILE * FileConfig;
-void ExtractConfigFile (STATE * S)
+void ExtractConfigFile (STATE * S, MATRIKS * M)
 {
-	// int TPeta, LPeta;
-	// int NbBuilding;
-	// int i, j;
-	// char read;
-	// char file_name[25];
+	int TPeta, LPeta;
+	int NbBuilding;
+	int i, j;
+	char read;
+	char file_name[25];
 
-    // /* Algoritma */
-    // printf("%s","Masukkan nama file: " );
-    // scanf(file_name);
-    // FileConfig = fopen(file_name,"r");
+    /* Algoritma */
+    printf("%s","Masukkan nama file: " );
+    scanf(file_name);
+    FileConfig = fopen(file_name,"r");
 	
-	// TPeta = CKata;
-	// ADVKATA;
-	// LPeta = CKata;
+	TPeta = CKata;
+	ADVKATA;
+	LPeta = CKata;
 
-	// /* Membaca jumlah Building dari FileConfig*/
-	// ADVKATA; //ENTER LINE
-	// NbBuilding = CKata;
-	// ADVKATA; //ENTER LINE
+	/* Membaca jumlah Building dari FileConfig*/
+	ADVKATA; // ENTER LINE
+	NbBuilding = CKata;
+	ADVKATA; // ENTER LINE
 
-	// /* Untuk membaca informasi mengenai posisi Building */
-	// for (i=1;i<=NbBuilding;i++)
-	// {
+	/* Untuk membaca informasi mengenai posisi Building */
+	for (i=1;i<=NbBuilding;i++)
+	{
+		//baca jenis bangunan
+		ADVKATA;
+		//baca baris karakter
+		ADVKATA;
+		//baca kolom karakter
+		ADVKATA; // ENTER LINE
+	}
 
-	// }
+	/* Untuk membaca informasi informasi mengenai graf keterhubungan tiap pasang bangunan dari matriks dalam FileConfig */
+	for (i=1;i<=NbBuilding;i++)
+	{
+		for (j=1;j<=NbBuilding;j++)
+		{
 
-	// /* Untuk membaca informasi informasi mengenai graf keterhubungan tiap pasang bangunan dari matriks dalam FileConfig */
-	// for (i=1;i<=NbBuilding;i++)
-	// {
-	// 	for (j=1;j<=NbBuilding;j++)
-	// 	{
-
-	// 	}
-	// }
+		}
+	}
 }
 
 /* *** Selektor: Untuk sebuah File "configFile" yang terdefinisi: *** */
