af35c74b99c745f6fd5ed9ca7ef721d5705fb88e
Annisa <nisaprmst@gmail.com>
2019-11-14 22:19:12 +0700

    adt graph

--- include/graph.h
+++ include/graph.h
@@ -6,23 +6,64 @@
 
 #include "boolean.h"
 #include "listlinier.h"
-#include <stdio.h>
-#include <stdlib.h>
+
 
 // Maksimum simpul dari graph = maks bangunan
 #define N  600
 
 // Struktur data pake list of list jancok
-typedef List infotypeGraph;
 typedef struct tElmtgraph *addressGraph;
 typedef struct tElmtgraph {
-	infotypeGraph info;
+	infotypeList info;
+	List link;
 	addressGraph next;
 } ElmtGraph;
 typedef struct {
-	addressGraph FirstGraph;
+	addressGraph First;
 } Graph;
 
+/* Definisi graph :
+   Graph kosong : First(L) = Nil
+   Setiap elemen dengan addressGraph P dapat diacu Info(P), Next(P)
+   Elemen terakhir graph : jika addressnya Last, maka NextGraph(Last)=Nil */
 
+/*	Penggunaan pada tubes:
+	Graph digunakan untuk menyimpan indeks dari building pada array of building.
+	contoh = graph untuk jumlah building 4
+	file config =
+   			0 1 1 1
+   			1 0 0 0
+   			1 0 0 1
+   			1 0 1 0
+	graph yang terjadi
+		1 2->3->4
+		2 1
+		3 1->4
+		4 1->3
+*/
+
+#define InfoGraph(P) (P)->info
+#define NextGraph(P) (P)->next
+#define Link(P) 	 (P)->link
+#define FirstGraph(G) ((G).First)
+
+void initGraph(Graph *G, int n);
+/* I. S. Graph G sembarang, n adalah jumlah building/ jumlah elemen parent list
+   F. S. Terbentuk Graph n elemen dan link tiap elemen berupa empty list */
+
+void AddLink(Graph *G, infotypeList n, infotypeList p);
+/* I. S. Graph G terdefinisi, n dan p pasti tidak lebih dari jumlah building
+   F. S. Link dari elemen n bertambah dengan p, dan sebaliknya (soalnya bolak balik)
+   contoh :
+   1 2->3
+   2 1
+   3 1
+   Add(&G, 2, 3) 
+   1 2->3
+   2 1->3
+   3 1->3 */
+
+boolean IsLinked(Graph *G, infotypeList n, infotypeList p);
+/* Mengembalikan true jika pada link n ada p */
 
 #endif
